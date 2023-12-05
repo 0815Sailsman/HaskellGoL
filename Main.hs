004d5559d0304e37d55886f8c831dfc2bf40c03e
Straczi <k.arendt0302@gmail.com>
2023-12-05 23:07:04 +0100

    allow to get board from file

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import GoLAlgorithm
 import Cell
+import System.IO
 
 board:: [Cell]
 -- for testing : delete later
@@ -7,13 +8,34 @@ board = [
     Cell 0 0 True,
     Cell 0 1 True, 
     Cell 0 2 True]
+
+
 main :: IO ()
-
 main = do
-  putStrLn (displayBoard board (-10, -10) (20, 20))
+    contents <- readFile "board2.txt"
+    playRound (createBoardFromString contents)
 
+
+playRound:: [Cell] -> IO ()
+playRound cl = do 
+    putStrLn (displayBoard cl (-25, -25) (50, 50))
+    input <- getLine
+    playRound (gameOfLife cl)
 
 displayBoard :: [Cell] -> (Integer, Integer) -> (Integer, Integer) -> String
-displayBoard cl (x,y) (height, width) = concat [ (concat [getTileFill (xco, yco) | xco <- [x..(x+width)]]) ++ "\n" | yco <- [y..(y+height)] ]
+displayBoard cl (x,y) (height, width) = concat [ (concat [getTileFill (xco, yco) | xco <- [x..(x+width-1)]]) ++ "\n" | yco <- [y..(y+height-1)] ]
     where getTileFill :: (Integer, Integer) -> String
           getTileFill (xTF,yTF) = if Cell xTF yTF True `elem` cl then "██" else "░░"
+
+
+createBoardFromString:: String -> [Cell] 
+createBoardFromString inString = [  let x = read (head (splitOn (=='|') word))::Integer
+                                        y = read (last (splitOn (=='|') word))::Integer 
+                                    in Cell x y True 
+                                    | word <- words inString]
+
+splitOn :: (Char -> Bool) -> String -> [String]
+splitOn p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : splitOn p s''
+                            where (w, s'') = break p s'
