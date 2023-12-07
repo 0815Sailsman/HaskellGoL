89f0aa5872d596fefd37476917ca978770254811
Straczi <k.arendt0302@gmail.com>
2023-12-07 11:02:12 +0100

    add (not important) file benchmark that allows to run the gol algorithm multiple times to check how long it takes

---
+++ benchmark.hs
@@ -0,0 +1,37 @@
+import GoLAlgorithm
+import Cell
+import System.IO
+
+
+main :: IO ()
+main = do
+    contents <- readFile "board1.txt"
+    playRound (createBoardFromString contents)
+
+
+playRound:: [Cell] -> IO ()
+playRound cl = do
+    putStrLn (playIRounds cl 50)
+
+playIRounds :: (Num t, Eq t) => [Cell] -> t -> String
+playIRounds cl 0 = displayBoard cl (-25, -25) (50, 50)
+playIRounds cl i = playIRounds (gameOfLife cl) (i-1)
+
+
+
+createBoardFromString:: String -> [Cell] 
+createBoardFromString inString = [  let x = read (head (splitOn (=='|') word))::Integer
+                                        y = read (last (splitOn (=='|') word))::Integer 
+                                    in Cell x y True 
+                                    | word <- words inString]
+
+splitOn :: (Char -> Bool) -> String -> [String]
+splitOn p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : splitOn p s''
+                            where (w, s'') = break p s'
+
+displayBoard :: [Cell] -> (Integer, Integer) -> (Integer, Integer) -> String
+displayBoard cl (x,y) (height, width) = concat [ (concat [getTileFill (xco, yco) | xco <- [x..(x+width-1)]]) ++ "\n" | yco <- [y..(y+height-1)] ]
+    where getTileFill :: (Integer, Integer) -> String
+          getTileFill (xTF,yTF) = if Cell xTF yTF True `elem` cl then "██" else "░░"
